7c6d90dc75d575c36cd113afd4b53294dd826dba
Margherita Galeazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2023-06-06 14:49:57 +0200

    Code Cleared
    Do some clean up on the code

--- Assets/Scripts/PlayerSelection.cs
+++ Assets/Scripts/PlayerSelection.cs
@@ -19,37 +19,11 @@ public class PlayerSelection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         animator = GetComponent<Animator>();
-        if(!PlayerPrefs.HasKey("avatarSelected"))
-        {
-            avatarSelected=0;
-            avatar = Instantiate(avatarModels[0],new Vector3(0f,-3f,0f), new Quaternion(0f,180f,0,0)) as GameObject;
-            avatar.transform.parent=transform;
-            avatar.transform.localScale=new Vector3(0.46f,0.46f,0.46f);
-            // Set the animator controller
-            animator.runtimeAnimatorController = animatorController[avatarSelected];
-            // Set the avatar for animation
-            animator.avatar = avatarAnimation[avatarSelected];
-            animator.Play("selection");
-        }
-        else
-        {
-            Load();
-            avatar = Instantiate(avatarModels[avatarSelected],new Vector3(0f,-3f,0f), new Quaternion(0f,180f,0,0)) as GameObject;
-            avatar.transform.parent=transform;
-            avatar.transform.localScale=new Vector3(0.46f,0.46f,0.46f);
-            // Set the animator controller
-            animator.runtimeAnimatorController = animatorController[avatarSelected];
-            // Set the avatar for animation
-            animator.avatar = avatarAnimation[avatarSelected];
-            animator.Play("selection");
-        }
+        int avatarSelected = PlayerPrefs.HasKey("avatarSelected") ? PlayerPrefs.GetInt("avatarSelected") : 0;
+        avatar=Instantiate(avatarModels[avatarSelected],transform.position,transform.rotation) as GameObject;
         UpdateAvatar(avatarSelected);
     }
-    /*void Update()
-    {
-        avatar.transform.Rotate(0f, 15*Time.deltaTime, 0f);
-    }*/
+    
     //Function to go to the next avatar (called when pressing next button)
     public void NextAvatar()
     {
@@ -67,7 +41,7 @@ public class PlayerSelection : MonoBehaviour
     {
         avatarSelected--;
         //check if we have reached the end of the avatar array
-        if (avatarSelected < 0)
+        if (avatarSelected <= 0)
         {
             avatarSelected = avatarModels.Length;
         }
@@ -82,16 +56,10 @@ public class PlayerSelection : MonoBehaviour
         thisModel.transform.parent = transform;
         thisModel.transform.localScale=new Vector3(0.46f,0.46f,0.46f);
         avatar = thisModel;
-        //animator = GetComponent<Animator>();
-         // Set the animator controller
-        animator.runtimeAnimatorController = animatorController[avatarSelected];
-        // Set the avatar for animation
-        animator.avatar = avatarAnimation[avatarSelected];
+        animator = GetComponent<Animator>();
+        animator.runtimeAnimatorController = animatorController[avatarSelected];// Set the animator controller
+        animator.avatar = avatarAnimation[avatarSelected];// Set the avatar for animation
         animator.Play("selection");
-    }
-    private void Load()
-    {
-        avatarSelected=PlayerPrefs.GetInt("avatarSelected");
     }
     private void Save()
     {
