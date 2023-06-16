f3075f32cc4994034119ffcfba2d0978a1b1b405
Margherita Galeazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2023-06-16 12:59:33 +0200

    Subtitle Typewriter effects

--- Assets/Scripts/SubtitleTrackMixer.cs
+++ Assets/Scripts/SubtitleTrackMixer.cs
@@ -1,10 +1,10 @@
-using TMPro;
+/*using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class SubtitleTrackMixer : PlayableBehaviour
 {
-    public string subtitleText;
+    //public string subtitleText;
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
@@ -32,6 +32,70 @@ public class SubtitleTrackMixer : PlayableBehaviour
        }
        }
        text.text = currentText;
-       text.color=new Color(currentRcolor,currentGcolor,currentBcolor,info.weight);
+       text.color=new Color(currentRcolor,currentGcolor,currentBcolor,currentAlpha);
+    }
+}*/
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SubtitleTrackMixer : PlayableBehaviour
+{
+    private TextMeshProUGUI text;
+    private string targetText;
+    private float targetRcolor;
+    private float targetGcolor;
+    private float targetBcolor;
+    private float targetAlpha;
+    private float typingSpeed = 20f; // Adjust typing speed as desired
+    private float elapsedTypingTime = 0f;
+    private int visibleCharacterCount = 0;
+    private string printedText= "";
+    
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        text = playerData as TextMeshProUGUI;
+        if (!text)
+            return;
+
+
+        int inputCount = playable.GetInputCount();
+        for (int i = 0; i < inputCount; i++)
+        {
+            float inputWeight = playable.GetInputWeight(i);
+            if (inputWeight > 0f)
+            {
+                ScriptPlayable<SubtitleBehavior> inputPlayable = (ScriptPlayable<SubtitleBehavior>)playable.GetInput(i);
+                SubtitleBehavior input = inputPlayable.GetBehaviour();
+                targetText = input.subtitleText;
+                targetRcolor = input.rcolor;
+                targetGcolor = input.gcolor;
+                targetBcolor = input.bcolor;
+                targetAlpha = inputWeight;
+            }
+        }
+        //visibleCharacterCount=0;
+        if (targetText != text.text && targetText!=printedText)
+        {
+            elapsedTypingTime += (float)info.deltaTime;
+            if (elapsedTypingTime >= 1f / typingSpeed)
+                {
+                    visibleCharacterCount++;
+                    elapsedTypingTime = 0f;
+                    printedText = targetText.Substring(0, visibleCharacterCount);
+                }
+
+            if (visibleCharacterCount >= targetText.Length)
+                {
+                    visibleCharacterCount = targetText.Length;
+                    visibleCharacterCount=0;
+                }
+
+        }
+
+        text.text = targetText.Substring(0, visibleCharacterCount);
+        text.color = new Color(targetRcolor, targetGcolor, targetBcolor, targetAlpha);
     }
 }
