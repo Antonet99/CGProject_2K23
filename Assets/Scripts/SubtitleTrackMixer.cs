5d37b43007bb93f5deb98ab73b43156d3d357741
Margherita Galeazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2023-06-16 14:32:30 +0200

    Subltitle typewriter

--- Assets/Scripts/SubtitleTrackMixer.cs
+++ Assets/Scripts/SubtitleTrackMixer.cs
@@ -1,41 +1,3 @@
-/*using TMPro;
-using UnityEngine;
-using UnityEngine.Playables;
-
-public class SubtitleTrackMixer : PlayableBehaviour
-{
-    //public string subtitleText;
-
-    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
-    {
-       TextMeshProUGUI text = playerData as TextMeshProUGUI;
-       string currentText = "";
-       float currentRcolor = 1;
-       float currentGcolor = 1;
-       float currentBcolor = 1;
-       float currentAlpha =0f;
-       
-       if(!text) { return; }
-
-       int inputCount = playable.GetInputCount();
-       for(int i = 0; i < inputCount; i++) {
-       float inputWeight = playable.GetInputWeight(i);
-       if(inputWeight>0f)
-       {
-        ScriptPlayable<SubtitleBehavior> inputPlayable=(ScriptPlayable<SubtitleBehavior>)playable.GetInput(i);
-        SubtitleBehavior input = inputPlayable.GetBehaviour();
-        currentText = input.subtitleText;
-        currentRcolor = input.rcolor;
-        currentGcolor = input.gcolor;
-        currentBcolor = input.bcolor;
-        currentAlpha=inputWeight;
-       }
-       }
-       text.text = currentText;
-       text.color=new Color(currentRcolor,currentGcolor,currentBcolor,currentAlpha);
-    }
-}*/
-
 using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -43,16 +5,9 @@ using UnityEngine.Playables;
 public class SubtitleTrackMixer : PlayableBehaviour
 {
     private TextMeshProUGUI text;
-    private string targetText;
-    private float targetRcolor;
-    private float targetGcolor;
-    private float targetBcolor;
-    private float targetAlpha;
-    private float typingSpeed = 20f; // Adjust typing speed as desired
-    private float elapsedTypingTime = 0f;
-    private int visibleCharacterCount = 0;
-    private string printedText= "";
-    
+    private string targetText, printedText= "";
+    private float targetRcolor,targetGcolor,targetBcolor,targetAlpha,typingSpeed = 20f,elapsedTypingTime = 0f;
+    private int visibleCharacterCount = 0; 
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
@@ -69,7 +24,7 @@ public class SubtitleTrackMixer : PlayableBehaviour
             {
                 ScriptPlayable<SubtitleBehavior> inputPlayable = (ScriptPlayable<SubtitleBehavior>)playable.GetInput(i);
                 SubtitleBehavior input = inputPlayable.GetBehaviour();
-                targetText = input.subtitleText;
+                targetText = input.subtitleText+"              ";
                 targetRcolor = input.rcolor;
                 targetGcolor = input.gcolor;
                 targetBcolor = input.bcolor;
@@ -92,10 +47,10 @@ public class SubtitleTrackMixer : PlayableBehaviour
                     visibleCharacterCount = targetText.Length;
                     visibleCharacterCount=0;
                 }
-
         }
 
         text.text = targetText.Substring(0, visibleCharacterCount);
         text.color = new Color(targetRcolor, targetGcolor, targetBcolor, targetAlpha);
+
     }
 }
