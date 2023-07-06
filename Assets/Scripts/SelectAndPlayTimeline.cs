3fd977d05615edd8cf351759b355eb7c7f603895
Margherita Galeazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2023-07-06 07:52:45 +0200

    CutScene Female Done

--- Assets/Scripts/SelectAndPlayTimeline.cs
+++ Assets/Scripts/SelectAndPlayTimeline.cs
@@ -6,22 +6,48 @@ public class SelectAndPlayTimeline : MonoBehaviour
 {
     public PlayableDirector[] directors;
     private int avatarSelected;
-    private PlayableDirector director;
     public GameObject[] player;
+     private bool timelinePlaying;
 
-    private void Awake()
+     private void Start()
     {
         avatarSelected=PlayerPrefs.GetInt("avatarSelected");
-        player[avatarSelected].SetActive(true);
-        director = directors[avatarSelected];
-        director.stopped += OnTimelineStopped;
-        director.Play();
+        if (avatarSelected==0)
+        {
+            StartTimeline(directors[0]);
+        }
+        else
+        {
+            StartTimeline(directors[1]);
+        }
+    }
+
+    private void StartTimeline(PlayableDirector timeline)
+    {
+        timeline.stopped += OnTimelineStopped;
+        timeline.Play();
+        timelinePlaying = true;
+    }
+
+     private void Update()
+    {
+        if (timelinePlaying)
+        {
+            if (directors[0].state != PlayState.Playing)
+            {
+                OnTimelineStopped(directors[0]);
+            }
+            if(directors[1].state != PlayState.Playing)
+            {
+                OnTimelineStopped(directors[1]);
+            }
+        }
     }
 
     private void OnTimelineStopped(PlayableDirector director)
     {
-        SceneManager.LoadScene(2);
         // Code to return to the game when the timeline is ended
-        // Add your implementation here
+        Debug.Log("Timeline stopped");
+        SceneManager.LoadScene(2);
     }
 }
